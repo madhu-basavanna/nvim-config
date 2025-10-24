6961495d8ffa3a4ea2de0d5ceea5b84a4afa2217
Madhu Basavanna <mb@aiconix.ai>
2025-10-24 19:24:41 +0200

    Added ctrl + s and ctrl + q to save and quit and removed oil plugin key binding since its no longer used

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,10 +1,26 @@
-vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", {desc="Open Parent Directory in Oil"})
-vim.keymap.set("n", "gl", function()
-    vim.diagnostic.open_float()
+local keymap = vim.keymap.set
+
+keymap("n", "gl", function()
+	vim.diagnostic.open_float()
 end, { desc = "Open Diagnostics in Float" })
 
-vim.keymap.set("n", "<leader>cf", function()
-    require("conform").format({
-        lsp_format = "fallback",
-    })
+keymap("n", "<leader>cf", function()
+	require("conform").format({
+		lsp_format = "fallback",
+	})
 end, { desc = "Format current file" })
+
+-- Save functionality
+keymap({ "n", "i" }, "<C-s>", function()
+	vim.cmd("w")
+end, { desc = "Save file" })
+
+-- Quit functionality
+keymap({ "n", "i" }, "<C-q>", function()
+	vim.cmd("q")
+end, { desc = "Quit Neovim" })
+
+-- Save and quit
+keymap({ "n", "i" }, "<C-w>", function()
+	vim.cmd("wq")
+end, { desc = "Save and quit" })
