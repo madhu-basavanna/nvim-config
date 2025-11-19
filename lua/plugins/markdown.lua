c5411c865219f454a38333cb7bfcf53854ac5a09
Madhu Basavanna <mb@aiconix.ai>
2025-11-19 12:01:26 +0100

    Updated plugins and add options to display icons, links in markdown

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -9,5 +9,41 @@ return {
 		completions = {
 			lsp = { enabled = true },
 		},
+		render_modes = true,
+		link = {
+			enabled = true,
+			render_modes = false,
+			footnote = {
+				enabled = true,
+				icon = "󰯔 ",
+				superscript = true,
+				prefix = "",
+				suffix = "",
+			},
+			image = "󰥶 ",
+			email = "󰀓 ",
+			hyperlink = "󰌹 ",
+			highlight = "RenderMarkdownLink",
+			wiki = {
+				icon = "󱗖 ",
+				body = function()
+					return nil
+				end,
+				highlight = "RenderMarkdownWikiLink",
+				scope_highlight = nil,
+			},
+			custom = {
+				web = { pattern = "^http", icon = "󰖟 " },
+				discord = { pattern = "discord%.com", icon = "󰙯 " },
+				github = { pattern = "github%.com", icon = "󰊤 " },
+				gitlab = { pattern = "gitlab%.com", icon = "󰮠 " },
+				google = { pattern = "google%.com", icon = "󰊭 " },
+				neovim = { pattern = "neovim%.io", icon = " " },
+				reddit = { pattern = "reddit%.com", icon = "󰑍 " },
+				stackoverflow = { pattern = "stackoverflow%.com", icon = "󰓌 " },
+				wikipedia = { pattern = "wikipedia%.org", icon = "󰖬 " },
+				youtube = { pattern = "youtube%.com", icon = "󰗃 " },
+			},
+		},
 	},
 }
