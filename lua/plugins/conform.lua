bc707db1f11cb7a3b9d87c46904b39430dcb5475
Madhu Basavanna <mb@aiconix.ai>
2025-11-21 07:09:13 +0100

    removed leader cf to format code since i forget to save if i have that so the code formatter formats once i save

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,17 +1,5 @@
 return {
 	"stevearc/conform.nvim",
-	event = { "BufWritePre" },
-	cmd = { "ConformInfo" },
-	keys = {
-		{
-			"<leader>cf",
-			function()
-				require("conform").format({ async = true, lsp_fallback = true })
-			end,
-			mode = "",
-			desc = "[C]ode [F]ormat",
-		},
-	},
 	opts = {
 		formatters_by_ft = {
 			lua = { "stylua" },
