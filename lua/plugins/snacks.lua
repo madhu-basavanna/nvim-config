016257c2032c979585f36f35df0c210f0ab6c60f
Madhu Basavanna <mb@aiconix.ai>
2025-11-25 05:12:24 +0100

    Removed fzf dependency and using snacks instead of it to make the config minimal with less plugins

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -73,11 +73,25 @@ return {
 		},
 		-- find
 		{
-			"<leader>fg",
+			"<leader>fb",
 			function()
-				Snacks.picker.git_files()
+				Snacks.picker.buffers()
 			end,
-			desc = "Find Git Files",
+			desc = "Buffers",
+		},
+		{
+			"<leader>fc",
+			function()
+				Snacks.picker.files({ cwd = vim.fn.stdpath("config") })
+			end,
+			desc = "Find Config File",
+		},
+		{
+			"<leader>ff",
+			function()
+				Snacks.picker.files()
+			end,
+			desc = "Find Files",
 		},
 		{
 			"<leader>fp",
@@ -92,56 +106,6 @@ return {
 				Snacks.picker.recent()
 			end,
 			desc = "Recent",
-		},
-		-- git
-		{
-			"<leader>gb",
-			function()
-				Snacks.picker.git_branches()
-			end,
-			desc = "Git Branches",
-		},
-		{
-			"<leader>gl",
-			function()
-				Snacks.picker.git_log()
-			end,
-			desc = "Git Log",
-		},
-		{
-			"<leader>gL",
-			function()
-				Snacks.picker.git_log_line()
-			end,
-			desc = "Git Log Line",
-		},
-		{
-			"<leader>gs",
-			function()
-				Snacks.picker.git_status()
-			end,
-			desc = "Git Status",
-		},
-		{
-			"<leader>gS",
-			function()
-				Snacks.picker.git_stash()
-			end,
-			desc = "Git Stash",
-		},
-		{
-			"<leader>gd",
-			function()
-				Snacks.picker.git_diff()
-			end,
-			desc = "Git Diff (Hunks)",
-		},
-		{
-			"<leader>gf",
-			function()
-				Snacks.picker.git_log_file()
-			end,
-			desc = "Git Log File",
 		},
 		-- Grep
 		{
@@ -450,13 +414,6 @@ return {
 				Snacks.terminal()
 			end,
 			desc = "Toggle Terminal",
-		},
-		{
-			"<c-_>",
-			function()
-				Snacks.terminal()
-			end,
-			desc = "which_key_ignore",
 		},
 		{
 			"]]",
