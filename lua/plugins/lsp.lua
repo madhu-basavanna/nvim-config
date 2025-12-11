181fbcc12f069bea0ef9a3c0f8301c4af42fb174
Madhu Basavanna <mb@aiconix.ai>
2025-12-11 13:36:45 +0100

    Added auto compilition plugin blink.cmp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,6 +5,7 @@ return {
 		-- Automatically install LSPs and related tools to stdpath for Neovim
 		-- Mason must be loaded before its dependents so we need to set it up here.
 		-- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
+		{ "saghen/blink.cmp" },
 		{ "williamboman/mason.nvim", opts = {} },
 		"williamboman/mason-lspconfig.nvim",
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
