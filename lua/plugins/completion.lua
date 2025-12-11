b0152c290f02d682a3624dca30110c7df9caa940
Madhu Basavanna <mb@aiconix.ai>
2025-12-11 15:04:13 +0100

    enabled signature for autocomplition and also docs

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -33,8 +33,21 @@ return {
 			nerd_font_variant = "mono",
 		},
 
+		-- Enable signature help - THIS IS THE KEY FEATURE YOU NEED
+		signature = {
+			enabled = true,
+		},
+
+		-- Enable auto-show documentation
+		completion = {
+			documentation = {
+				auto_show = true,
+				auto_show_delay_ms = 200,
+			},
+		},
+
 		-- (Default) Only show the documentation popup when manually triggered
-		completion = { documentation = { auto_show = false } },
+		-- completion = { documentation = { auto_show = false } },
 
 		-- Default list of enabled providers defined so that you can extend it
 		-- elsewhere in your config, without redefining it, due to `opts_extend`
