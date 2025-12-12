9470af677815482b1ff01d928a9beebc689f1c9b
Madhu Basavanna <mb@aiconix.ai>
2025-12-12 07:45:39 +0100

    Updated plugins and replaced tokio night theme with vscode theme

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,13 +1,14 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  -- looad the colorscheme here
-  config = function()
-    require("tokyonight").setup({
-      style = "night", -- Use "day" for the light variant
-      -- other config options here if needed
-    })
-    vim.cmd([[colorscheme tokyonight-night]])
-  end,
+	"folke/tokyonight.nvim",
+	lazy = false,
+	enabled = false,
+	priority = 1000,
+	-- looad the colorscheme here
+	config = function()
+		require("tokyonight").setup({
+			style = "night", -- Use "day" for the light variant
+			-- other config options here if needed
+		})
+		vim.cmd([[colorscheme tokyonight-night]])
+	end,
 }
