c4dc47f2d0e135d125c83c4dd96f855b26343594
Madhu Basavanna <mb@aiconix.ai>
2026-01-29 12:30:38 +0100

    Updating the py-requirements plugin but it's still not working

--- lua/plugins/py-requirements.lua
+++ lua/plugins/py-requirements.lua
@@ -1,6 +1,7 @@
 return {
 	"MeanderingProgrammer/py-requirements.nvim",
 	dependencies = { "nvim-treesitter/nvim-treesitter" },
+	ft = "requirements",
 	config = function()
 		require("py-requirements").setup({})
 	end,
