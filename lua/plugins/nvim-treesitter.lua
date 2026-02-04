5c4509e242b9b377b58e35a0757543d65da80eb1
Madhu Basavanna <mb@aiconix.ai>
2026-02-04 08:56:24 +0100

    updated plugins

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,24 +1,37 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function () 
-        local configs = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
 
-        configs.setup({
-            ensure_installed = { "c", "lua", "python", "dockerfile", "vim", "vimdoc", "bash", "diff", "html", "luadoc", "markdown", "markdown_inline" },
-            sync_install = false,
-            highlight = { enable = true },
-            indent = { enable = true },
+		configs.setup({
+			ensure_installed = {
+				"c",
+				"lua",
+				"python",
+				"dockerfile",
+				"vim",
+				"vimdoc",
+				"bash",
+				"diff",
+				"html",
+				"luadoc",
+				"markdown",
+				"markdown_inline",
+			},
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
 
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "<Enter>", -- set to `false` to disable one of the mappings
-                    node_incremental = "<Enter>",
-                    scope_incremental = false,
-                    node_decremental = "<Backspace>",
-                },
-            },
-        })
-    end
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<Enter>", -- set to `false` to disable one of the mappings
+					node_incremental = "<Enter>",
+					scope_incremental = false,
+					node_decremental = "<Backspace>",
+				},
+			},
+		})
+	end,
 }
