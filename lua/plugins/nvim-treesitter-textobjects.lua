577a84e84337a0c7bfa89a2219da09dc3564dcb1
Madhu Basavanna <madhupatel135@gmail.com>
2026-07-07 12:02:41 +0200

    Removed copilot

--- lua/plugins/nvim-treesitter-textobjects.lua
+++ lua/plugins/nvim-treesitter-textobjects.lua
@@ -1,69 +1,63 @@
--- ~/.config/nvim/lua/plugins/treesitter-textobjects.lua
 return {
-	"nvim-treesitter/nvim-treesitter-textobjects",
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter"
+    },
+    init = function()
+        local config = require'nvim-treesitter.configs';
+        config.setup({
+            textobjects = {
+                select = {
+                    enable = true,
 
-	-- Make sure the core parser plugin is loaded first
-	dependencies = { "nvim-treesitter/nvim-treesitter" },
+                    -- Automatically jump forward to textobj, similar to targets.vim
+                    lookahead = true,
 
-	-- Load when any buffer that uses treesitter is opened
-	event = { "BufReadPost", "BufNewFile" },
-
-	-- **Tell lazy NOT to call the default `setup`**
-	config = false,
-
-	-- Provide our own config that forwards the table to the core plugin
-	opts = function()
-		return {
-			textobjects = {
-				select = {
-					enable = true,
-					lookahead = true,
-					keymaps = {
-						["af"] = "@function.outer",
-						["if"] = "@function.inner",
-						["ac"] = "@class.outer",
-						["ao"] = "@comment.outer",
-						["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-						["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
-					},
-					selection_modes = {
-						["@parameter.outer"] = "v",
-						["@function.outer"] = "V",
-						["@class.outer"] = "<c-v>",
-					},
-					include_surrounding_whitespace = true,
-				},
-
-				swap = {
-					enable = true,
-					swap_next = {
-						["<leader>a"] = { query = "@parameter.inner", desc = "Swap with next parameter" },
-					},
-					swap_previous = {
-						["<leader>A"] = "@parameter.inner",
-					},
-				},
-
-				move = {
-					enable = true,
-					set_jumps = true,
-					goto_next_start = {
-						["]f"] = "@function.outer",
-						["]c"] = "@class.outer",
-					},
-					goto_previous_start = {
-						["[f"] = "@function.outer",
-						["[c"] = "@class.outer",
-					},
-				},
-			},
-		}
-	end,
-
-	-- This is the *only* place we call `setup`
-	config = function(_, opts)
-		-- The core plugin will merge this into its own config if it has already
-		-- been loaded; otherwise we load it now.
-		require("nvim-treesitter.configs").setup(opts)
-	end,
+                    keymaps = {
+                        -- You can use the capture groups defined in textobjects.scm
+                        ["af"] = "@function.outer",
+                        ["if"] = "@function.inner",
+                        ["ac"] = "@class.outer",
+                        ["ao"] = "@comment.outer",
+                        -- You can optionally set descriptions to the mappings (used in the desc parameter of
+                        -- nvim_buf_set_keymap) which plugins like which-key display
+                        ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+                        -- You can also use captures from other query groups like `locals.scm`
+                        ["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
+                    },
+                    -- You can choose the select mode (default is charwise 'v')
+                    --
+                    -- Can also be a function which gets passed a table with the keys
+                    -- * query_string: eg '@function.inner'
+                    -- * method: eg 'v' or 'o'
+                    -- and should return the mode ('v', 'V', or '<c-v>') or a table
+                    -- mapping query_strings to modes.
+                    selection_modes = {
+                        ['@parameter.outer'] = 'v', -- charwise
+                        ['@function.outer'] = 'V', -- linewise
+                        ['@class.outer'] = '<c-v>', -- blockwise
+                    },
+                    -- If you set this to `true` (default is `false`) then any textobject is
+                    -- extended to include preceding or succeeding whitespace. Succeeding
+                    -- whitespace has priority in order to act similarly to eg the built-in
+                    -- `ap`.
+                    --
+                    -- Can also be a function which gets passed a table with the keys
+                    -- * query_string: eg '@function.inner'
+                    -- * selection_mode: eg 'v'
+                    -- and should return true or false
+                    include_surrounding_whitespace = true,
+                },
+                swap = {
+                    enable = true,
+                    swap_next = {
+                        ["<leader>a"] = {query="@parameter.inner", desc="Swap with next parameter"},
+                    },
+                    swap_previous = {
+                        ["<leader>A"] = "@parameter.inner",
+                    },
+                },
+            },
+        });
+    end
 }
--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -2,7 +2,9 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	config = function()
-		require("nvim-treesitter").setup({
+		local configs = require("nvim-treesitter.configs")
+
+		configs.setup({
 			ensure_installed = {
 				"c",
 				"lua",
@@ -24,7 +26,7 @@ return {
 			incremental_selection = {
 				enable = true,
 				keymaps = {
-					init_selection = "<Enter>",
+					init_selection = "<Enter>", -- set to `false` to disable one of the mappings
 					node_incremental = "<Enter>",
 					scope_incremental = false,
 					node_decremental = "<Backspace>",
