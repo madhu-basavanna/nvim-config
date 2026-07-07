5507e359bb85c7c9ddc6180fb0153641bd42004a
Madhu Basavanna <madhupatel135@gmail.com>
2026-07-07 14:13:25 +0200

    Removed copilot, markdown lsp and marksman plugin

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -205,7 +205,7 @@ return {
 		--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 		local servers = {
 			-- bashls = {},
-			marksman = {},
+			-- marksman = {},
 			-- clangd = {},
 			-- rust_analyzer = {},
 			-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
